1d50bb66972f951304198deda68aacd63916b6fb
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-01-11 12:52:14 +0800

    feat: add getByLabelText, getByPlaceholderText, getByText, getByAltText, getByTitle and getByTestId in Application.Test.tsx, add Application to App.tsx

--- src/components/Application/Application.test.tsx
+++ src/components/Application/Application.test.tsx
@@ -15,10 +15,33 @@ describe('Application', () => {
     });
     expect(sectionHeading).toBeInTheDocument();
 
+    const paragraphElement = screen.getByText('All fields are mandatory');
+    expect(paragraphElement).toBeInTheDocument();
+
+    const closeElement = screen.getByTitle('close');
+    expect(closeElement).toBeInTheDocument();
+
+    const imageElement = screen.getByAltText('a person with a laptop');
+    expect(imageElement).toBeInTheDocument();
+
+    const customElement = screen.getByTestId('custom-element');
+    expect(customElement).toBeInTheDocument();
+
     const nameElement = screen.getByRole('textbox', {
       name: 'Name',
     });
     expect(nameElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByLabelText('Name', {
+      selector: 'input',
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText('Fullname');
+    expect(nameElement3).toBeInTheDocument();
+
+    const nameElement4 = screen.getByDisplayValue('Ron');
+    expect(nameElement4).toBeInTheDocument();
 
     const bioElement = screen.getByRole('textbox', {
       name: 'Bio',
@@ -30,6 +53,11 @@ describe('Application', () => {
 
     const termsElement = screen.getByRole('checkbox');
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      'I agree to the terms and conditions'
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole('button');
     expect(submitButtonElement).toBeInTheDocument();
--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+
 const Application = () => {
+  const [inputName, setInputName] = useState('');
+
+  console.log(inputName);
+
   return (
     <>
       <h1>Job application form</h1>
       <h2>Section 1</h2>
+      <p>All fields are mandatory</p>
+      <span title='close'>X</span>
+      <img src='https://via.placeholder.com/150' alt='a person with a laptop' />
+      <p data-testid='custom-element'>Custom HTML element</p>
       <form>
         <div>
           <label htmlFor='name'>Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            placeholder='Fullname'
+            value='Ron'
+            onChange={(e) => {
+              setInputName(e.target.value);
+            }}
+          />
           <label htmlFor='bio'>Bio</label>
           <input type='text' id='bio' />
         </div>
